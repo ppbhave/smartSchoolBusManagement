459888e27470229fc7ace03d7a3aed6c281d7bbe
Shree <Shree@DESKTOP-5U0PHAV>
2019-02-06 00:08:15 +0530

    Add project files.

---
+++ ssbmadmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ssbmadmin.Global" Language="C#" %>
---
+++ ssbmadmin/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ssbmadmin.Startup))]
+namespace ssbmadmin
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
